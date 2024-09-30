88c21a1ecd7a669721c9086fe56842f977c7e683
Chase Swick <chaseswick@gmail.com>
2024-09-30 07:07:10 -0400

    Fix name error

--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -1,23 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
-import { images } from '../../constants';
 import './About.scss';
 import { urlFor, client } from '../../client';
 
-const qualities = [
-  { title: 'Fullstack Web Development', description: 'Design of .NET and Java applications using respective technology stacks', igmUrl: images.about01 },
-  { title: 'UI/UX skills', description: 'Blazor, React, Angular, Javascript, Typescript', igmUrl: images.about02 },
-  { title: 'Backend skills', description: 'C#, EF Core, SQL Server, Azure, Java, Springboot, AWS', igmUrl: images.about03 }
-]
-
 const About = () => {
-  const [qualities, setQualities] = useState([]);
+  const [abouts, setAbouts] = useState([]);
 
   useEffect(() => {
-    const query = '*[_type == "qualities"]'
+    const query = '*[_type == "abouts"]'
 
     client.fetch(query)
-      .then((data) => setQualities(data))
+      .then((data) => setAbouts(data))
   }, []);
 
   return (
@@ -29,17 +22,17 @@ const About = () => {
       </h2>
 
       <div className="app__profiles">
-        {qualities.map((quality, index) => (
+        {abouts.map((about, index) => (
           <motion.div
             whileInView={{ opacity: 1}}
             whileHover={{ scale: 1.1 }}
             transition={{ duration: 0.5, type: 'tween' }}
             className="app__profile-item"
-            key={quality.title + index}
+            key={abouts.title + index}
           >
-            <img src={urlFor(quality.igmUrl)} alt={quality.title}></img>
-            <h2 className="bold-text" style={{ marginTop: 20 }}>{quality.title}</h2>
-            <p className="p-text" style={{ marginTop: 20 }}>{quality.description}</p>
+            <img src={urlFor(about.imgUrl)} alt={about.title}></img>
+            <h2 className="bold-text" style={{ marginTop: 20 }}>{about.title}</h2>
+            <p className="p-text" style={{ marginTop: 20 }}>{about.description}</p>
           </motion.div>
         ))}
       </div>
