5f1a7b4e715c564d936e8cf882426664ab818769
Chase Swick <chaseswick@gmail.com>
2024-10-02 03:38:26 -0400

    Update assets

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,26 +1,18 @@
 import email from '../assets/email.png';
-import mobile from '../assets/mobile.png';
 import api from '../assets/api.png';
-import cpp from '../assets/cpp.png';
 import css from '../assets/css.png';
-import figma from '../assets/figma.png';
-import flutter from '../assets/flutter.png';
 import git from '../assets/git.png';
-import graphql from '../assets/graphql.png';
 import html from '../assets/html.png';
 import javascript from '../assets/javascript.png';
-import mu5 from '../assets/mu5.png';
-import node from '../assets/node.png';
-import python from '../assets/python.png';
 import react from '../assets/react.png';
-import redux from '../assets/redux.png';
 import sass from '../assets/sass.png';
 import typescript from '../assets/typescript.png';
-import vue from '../assets/vue.png';
-
-import about01 from '../assets/about01.png';
-import about02 from '../assets/about02.png';
-import about03 from '../assets/about03.png';
+import angular from '../assets/angular.png';
+import sql from '../assets/sql.png';
+import java from '../assets/java.png';
+import blazor from '../assets/blazor.png';
+import csharp from '../assets/csharp.png';
+import netcore from '../assets/netcore.png';
 
 import profile from '../assets/profile.png';
 import circle from '../assets/circle.svg';
@@ -36,27 +28,20 @@ import spotify from '../assets/spotify.png';
 
 export default {
   email,
-  mobile,
   api,
-  cpp,
   css,
-  figma,
-  flutter,
   git,
-  graphql,
   html,
   javascript,
-  mu5,
-  node,
-  python,
   react,
-  redux,
   sass,
   typescript,
-  vue,
-  about01,
-  about02,
-  about03,
+  angular,
+  sql,
+  java,
+  blazor,
+  csharp,
+  netcore,
   profile,
   circle,
   logo,
