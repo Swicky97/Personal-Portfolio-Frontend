c9676e821895009e9cbc60e6fe747585b27a67a5
Chase Swick <chaseswick@gmail.com>
2024-10-03 06:57:11 -0400

    Remove unneeded assets

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,19 +1,8 @@
 import email from '../assets/email.png';
 import mobile from '../assets/mobile.png';
-import api from '../assets/api.png';
 import css from '../assets/css.png';
-import git from '../assets/git.png';
-import html from '../assets/html.png';
-import javascript from '../assets/javascript.png';
 import react from '../assets/react.png';
-import sass from '../assets/sass.png';
-import typescript from '../assets/typescript.png';
-import angular from '../assets/angular.png';
-import sql from '../assets/sql.png';
-import java from '../assets/java.png';
 import blazor from '../assets/blazor.png';
-import csharp from '../assets/csharp.png';
-import netcore from '../assets/netcore.png';
 
 import profile from '../assets/profile.png';
 import circle from '../assets/circle.svg';
@@ -22,21 +11,10 @@ import logo from '../assets/logo.png';
 export default {
   email,
   mobile,
-  api,
   css,
-  git,
-  html,
-  javascript,
   react,
-  sass,
-  typescript,
-  angular,
-  sql,
-  java,
   blazor,
-  csharp,
-  netcore,
   profile,
-  circle,
-  logo,
+  circle, 
+  logo
 };
