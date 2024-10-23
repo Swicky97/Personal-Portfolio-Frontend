cb8b8d2fced9c17ca5c043f4445e9a0ed8d894f7
Chase Swick <chaseswick@gmail.com>
2024-10-23 19:02:32 -0400

    Update overlay

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { About, Footer, Header, Skills, Work } from './container';
-import { Navbar } from './components';
+import { Navbar, NavigationDots, SocialMedia } from './components';
 import './App.scss';
 
 const app = () => {
   return (
     <div className='app'>
+      <SocialMedia />
+      <NavigationDots />
+
       <Navbar />
       <Header />
       <About />
